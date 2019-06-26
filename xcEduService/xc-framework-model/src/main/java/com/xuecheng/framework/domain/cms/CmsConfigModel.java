b6d4162f0f2f6fc8717d86c1a6766fc67e01360a
tanghao <tanghao2yx@163.com>
2019-06-26 08:46:52 +0800

    model

--- xcEduService/xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/CmsConfigModel.java
+++ xcEduService/xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/CmsConfigModel.java
@@ -1,10 +1,27 @@
 package com.xuecheng.framework.domain.cms;
 
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Map;
+
 /**
  * CmsConfigModel
  *
  * @author th
  * 2019/6/25 19:38
  **/
+@Data
+@ToString
 public class CmsConfigModel {
+
+    private String key;
+
+    private String name;
+
+    private String url;
+
+    private Map mapValue;
+
+    private String value;
 }
--- xcEduService/xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/CmsTemplate.java
+++ xcEduService/xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/CmsTemplate.java
@@ -1,10 +1,31 @@
 package com.xuecheng.framework.domain.cms;
 
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
+
 /**
  * CmsTemplate
  *
  * @author th
  * 2019/6/25 20:18
  **/
+@Data
+@ToString
+@Document(collection = "cms_template")
 public class CmsTemplate {
+
+    //站点ID
+    private String siteId;
+    //模板ID
+    @Id
+    private String templateId;
+    //模板名称
+    private String templateName;
+    //模板参数
+    private String templateParameter;
+    //模板文件Id
+    private String templateFileId;
 }
